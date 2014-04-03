aa717acd6c7209d73ca8ad2c05014a06cdb54fa3
MichaelAng <ma87834n@pace.edu>
2014-04-03 11:50:44 -0400

    Initial commit.

---
+++ src/com/example/ziara/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.ziara;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+	private ListView mainListView;
+	private ArrayAdapter<String> listAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+
+		// Find the ListView resource.
+		mainListView = (ListView) findViewById(R.id.list);
+
+		// Create and populate a List of planet names.
+		String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+				"Jupiter", "Saturn", "Uranus", "Neptune" };
+
+		// Create ArrayAdapter using the planet list.
+		listAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, planets);
+
+		// Set the ArrayAdapter as the ListView's adapter.
+		mainListView.setAdapter(listAdapter);
+
+		mainListView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				// ListView Clicked item index
+				int itemPosition = position;
+
+				// ListView Clicked item value
+				String itemValue = (String) mainListView
+						.getItemAtPosition(position);
+
+				// Show Alert
+				Toast.makeText(
+						getApplicationContext(),
+						"Position :" + itemPosition + "  ListItem : "
+								+ itemValue, Toast.LENGTH_SHORT).show();
+
+			}
+
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
